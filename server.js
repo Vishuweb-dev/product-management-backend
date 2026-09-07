6612db39388c48572cd8839f9bce29fc1a72dce3
Vishuweb-dev <vishal275vishal@gmail.com>
2026-09-07 11:37:58 +0530

    corrs

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const productRoutes = require("./src/routes/product.routes")
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin:[
+        "https://product-management-frontend-nine-bay.vercel.app",
+     "http://localhost:5173",],
     credentials: true
 }))
 connectDB();
